f2ce031722f7475af8ad31a248602ad90bfa9468
jaecheon <kzc0222@gmail.com>
2019-09-23 22:13:01 +0900

    add bcrypt hash to password
    비밀번호를 hash 암호화로 저장

--- api/routes/users.js
+++ api/routes/users.js
@@ -42,38 +42,51 @@ router.get('/all', (req, res) => {
  */
 router.post('/register', (req, res) => {
 
-    // 유저 등록 속성
-    const user = new userModel({
-        _id : new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-
-    user
-        .save()
-        .then(result => {
-            if (!result) {
-                return res.status(404).json({
-                    msg: 'Failed create user'
-                });
-            } else {
-                res.status(200).json({
-                    msg: 'Successful create user',
-                    userInfo: result,
-                    request: {
-                        type: 'GET',
-                        url: 'http://localhost:5000/users/all'
-                    }
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).json({
+    /**
+     * bcrypt 이용 비밀번호 hash 암호화
+     * password 값 첫 번째 아규먼트 암호화 할 값, 두번 째 아규먼트 자리수, 
+     * 3번 째 아규먼트 에러가 있으면 에러 표시, hash가 있으면 hash 표시
+     */
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        // 에러 일 때
+        if (err) {
+            return res.status(500).json({
                 error: err
             });
-        });
+        } else {
+            // 유저 등록 속성
+            const user = new userModel({
+                _id: new mongoose.Types.ObjectId(),
+                name: req.body.name,
+                email: req.body.email,
+                password: hash
+            });
 
+            user
+                .save()
+                .then(result => {
+                    if (!result) {
+                        return res.status(404).json({
+                            msg: 'Failed create user'
+                        });
+                    } else {
+                        res.status(200).json({
+                            msg: 'Successful create user',
+                            userInfo: result,
+                            request: {
+                                type: 'GET',
+                                url: 'http://localhost:5000/users/all'
+                            }
+                        });
+                    }
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    });
+                });
+        }
+    });
 });
 
 
