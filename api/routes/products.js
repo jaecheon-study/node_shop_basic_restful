c48bee89c041d274f8743a11896b693c6cac695b
jaecheon <kzc0222@gmail.com>
2019-09-23 11:08:38 +0900

    add more product routers (post, patch, delete)

--- api/routes/products.js
+++ api/routes/products.js
@@ -12,4 +12,38 @@ router.get('/', (req, res) => {
     });
 });
 
+/**
+ * @route   POST /products
+ * @desc    Test Post Router
+ * @access  Public
+ */
+router.post('/', (req, res) => {
+    res.status(200).json({
+        msg: 'Success post products test'
+    });
+});
+
+/**
+ * @route   PATCH /products
+ * @desc    Test Patch Router
+ * @access  Public
+ */
+router.patch('/', (req, res) => {
+    res.status(200).json({
+        msg: 'Success patch products test'
+    });
+});
+
+/**
+ * @route   DELETE /products
+ * @desc    Test Delete Router
+ * @access  Public
+ */
+router.delete('/', (req, res) => {
+    res.status(200).json({
+        msg: 'Success delete products test'
+    });
+});
+
+
 module.exports = router;
