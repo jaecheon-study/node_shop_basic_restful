8aede3b08138cc91d480a80c0450c4debd0bb600
jaecheon <kzc0222@gmail.com>
2019-09-23 22:22:41 +0900

    Check existing email and check email form
    기존 이메일 유무 및 이메일 양식 체크

--- api/models/user.js
+++ api/models/user.js
@@ -7,7 +7,13 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: {type: String},
-    email: {type: String, require: true},
+    email: {
+        type: String, 
+        require: true,
+        unique: true, // 유니크한 값. 하나만 존재하게. 기존 이메일 체크 유무
+        // 이메일 형식 정규식.
+        match:  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    },
     password: {type: String, require: true}
 });
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -42,52 +42,69 @@ router.get('/all', (req, res) => {
  */
 router.post('/register', (req, res) => {
 
-    /**
-     * bcrypt 이용 비밀번호 hash 암호화
-     * password 값 첫 번째 아규먼트 암호화 할 값, 두번 째 아규먼트 자리수, 
-     * 3번 째 아규먼트 에러가 있으면 에러 표시, hash가 있으면 hash 표시
-     */
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        // 에러 일 때
-        if (err) {
-            return res.status(500).json({
-                error: err
-            });
-        } else {
-            // 유저 등록 속성
-            const user = new userModel({
-                _id: new mongoose.Types.ObjectId(),
-                name: req.body.name,
-                email: req.body.email,
-                password: hash
-            });
-
-            user
-                .save()
-                .then(result => {
-                    if (!result) {
-                        return res.status(404).json({
-                            msg: 'Failed create user'
+    // 기존 이메일이 있는지 체크
+    userModel
+        .find({ email: req.body.email }) // 기존 이메일에서 찾는다.
+        .exec()
+        .then(email => {
+            // email의 길이가 1과 같거나 크면 기존 이메일이 있는 것.
+            if (email.length >= 1) {
+                return res.status(409).json({
+                    msg: 'please check your email (Email already exists)'
+                });
+            } else {
+                /**
+                 * bcrypt 이용 비밀번호 hash 암호화
+                 * password 값 첫 번째 아규먼트 암호화 할 값, 두번 째 아규먼트 자리수, 
+                 * 3번 째 아규먼트 에러가 있으면 에러 표시, hash가 있으면 hash 표시
+                 */
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    // 에러 일 때
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
                         });
                     } else {
-                        res.status(200).json({
-                            msg: 'Successful create user',
-                            userInfo: result,
-                            request: {
-                                type: 'GET',
-                                url: 'http://localhost:5000/users/all'
-                            }
+                        // 유저 등록 속성
+                        const user = new userModel({
+                            _id: new mongoose.Types.ObjectId(),
+                            name: req.body.name,
+                            email: req.body.email,
+                            password: hash
                         });
-                    }
-                })
-                .catch(err => {
-                    res.status(500).json({
-                        error: err
-                    });
-                });
-        }
-    });
-});
 
+                        user
+                            .save()
+                            .then(result => {
+                                if (!result) {
+                                    return res.status(404).json({
+                                        msg: 'Failed create user'
+                                    });
+                                } else {
+                                    res.status(200).json({
+                                        msg: 'Successful create user',
+                                        userInfo: result,
+                                        request: {
+                                            type: 'GET',
+                                            url: 'http://localhost:5000/users/all'
+                                        }
+                                    });
+                                }
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+});
 
 module.exports = router;
